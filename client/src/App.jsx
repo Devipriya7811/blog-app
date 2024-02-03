70fb4615767b75392df9d0db6e87f8fb5a4a7c6c
Devipriya7811 <devipriyas7811@gmail.com>
2024-02-03 18:56:35 +0530

    complete

---
+++ client/src/App.jsx
@@ -0,0 +1,56 @@
+
+import { Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import { Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import PostDetails from "./pages/PostDetails";
+import CreatePost from "./pages/CreatePost";
+import EditPost from "./pages/EditPost";
+import Profile from "./pages/Profile";
+import { UserContextProvider } from "./context/UserContext";
+import MyBlogs from "./pages/MyBlogs";
+
+
+const App = () => {
+  return(
+    
+      <UserContextProvider>
+      <Routes>
+        <Route 
+        path = "/" 
+        element = {<Home/>}/>
+
+        <Route 
+        path = "/login" 
+        element = {<Login/>}/>
+
+        <Route 
+        path = "/register" 
+        element = {<Register/>}/>
+
+        <Route 
+        path = "/write" 
+        element = {<CreatePost/>}/>
+
+        <Route 
+        path = "/posts/post/:id" 
+        element = {<PostDetails/>}/>
+
+        <Route 
+        path = "/edit/:id" 
+        element = {<EditPost/>}/>
+
+        <Route 
+        path = "/my blogs/:id" 
+        element = {<MyBlogs/>}/>
+
+        <Route 
+        path = "/profile/:id" 
+        element = {<Profile/>}/>
+      </Routes>
+      
+      </UserContextProvider>
+  )
+}
+export default App;
